fe1e6060c233e5e667029fec353c1b2f7da2e5e4
Jingjing Yin <hyesung_love1127@126.com>
2016-12-18 20:24:34 +0800

    locustfile.py: support to read and split large word table
    Add clinet id and number of clients
    
    Signed-off-by: Jingjing Yin <hyesung_love1127@126.com>

--- locustfile.py
+++ locustfile.py
@@ -1,29 +1,42 @@
 import sys
 
 import codecs
-from locust import HttpLocust, TaskSet, task, events
+from locust import HttpLocust, TaskSet, task
 
 headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-16LE'}
-URL_source = "./tupu_resin_0919-small"
+#URL_source = "./tupu_resin_0919-small"
+URL_source = "./tupu_resin_0919"
 class UserBehavior(TaskSet):
     def on_start(self, client_id, num_clients):
+        self.line_count = 0
         self.file = open(URL_source, "r")
+        self.client_id = client_id
+        self.num_clients = num_clients
         print("user %d, total count %d" % (client_id, num_clients))
+
 
     @task(1)
     def index(self):
-        line = self.file.readline()
-        if line:
-            line = line.rstrip('\n')
-        else:
-            self.file.close()
-            self.file = open(URL_source, "r")
+        while True:
             line = self.file.readline()
+            if not line:
+                # Read URL_sources again
+                self.file.close()
+                self.line_count = 0
+                self.file = open(URL_source, "r")
+                line = self.file.readline()
+
             line = line.rstrip('\n')
+            self.line_count += 1
+
+            # Each client/user only use its own, not conflict with others
+            if (self.line_count % self.num_clients == self.client_id % self.num_clients):
+                break
 
         response = self.client.post("/", headers=headers, data=line)
         if response.status_code != 200:
-            print "Response status code:", response.status_code
+            #print "User %d, %d, input %s" % (self.client_id, self.line_count, line)
+            #print "Response status code:", response.status_code
             content = response.content
             decoder = codecs.getdecoder(response.encoding)
             (content,length) = decoder(content)
